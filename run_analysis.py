3ced2c9ab4d125fb5be178f61f68295839ce78cc
kaf0005 <kaf0005@mix.wvu.edu>
2019-07-09 11:04:33 -0400

    analysis code

--- run_analysis.py
+++ run_analysis.py
@@ -3,17 +3,18 @@
 Example run script for BRER simulations
 """
 
-import run_brer.mem_search as rc
+import run_brer.mem_search as ms
 import sys
 
 
 init = {
     'ensemble_dir': '/home/kaf0005/KF2019REU/jmh_dc3_runs',
-    'pairs_json': '/home/kaf0005/KF2019REU/syntaxin_files/pair_data.json'
-    'analysis_dir':'/home/kaf0005/KF2019REU/analysis'
-}
+    'pairs_json': '/home/kaf0005/KF2019REU/syntaxin_files/pair_data.json',
+    'analysis_dir': '/home/kaf0005/KF2019REU/analysis',
+    'n':[1,2,3,4],
+    'm':[1,2]
+        }
 
 
-config = rc.RunConfig(**init)
+config = ms.Analysis(**init)
 config.run()
-
--- run_brer/mem_search.py
+++ run_brer/mem_search.py
@@ -1,51 +1,58 @@
 
 #!/usr/bin/env python3
-
-from copy import deepcopy
+from run_brer.run_data import RunData
+from run_brer.pair_data import MultiPair
 import os
-import shutil 
-import logging 
 import json 
-import atexit 
 import numpy as np 
 import sys
-import argparse 
-from run_brer.pair_data import MultiPair 
-from run_brer.run_brer.directory_helper import DirectoryHelper 
-import linecache
+
 
 class Analysis: 
 
     def __init__(self,
                 ensemble_dir,
-                work_dir,
-                current_dir,
                 analysis_dir,
-                n=[],
+                n,
+                m,
                 pairs_json='pair_data.json',
                 data=[],
                 W_matrix=[],
                 distance_values=[]
                 ):
 
-    ## Your working directory is where the mem_# files are
-    ## and the current directory is where the files from this script
-    ## will output which is where this script is currently 
-
-        self.current_dir = os.getcwd()
         self.ensemble_dir=ensemble_dir
         self.analysis_dir=analysis_dir
-        self.n=[]
+        self.n=n
+        self.m=m
         self.__names=[]
         self.pairs = MultiPair()
         self.pairs.read_from_json(pairs_json)
-        self.__names = self.pairs.get_names()
+        self.__names = self.pairs.names
         self.state_json = '{}/mem_{}/state.json'.format(self.ensemble_dir,self.n)
         self.data=[]
         self.W_matrix=[]
         self.distance_values=distance_values
 
-    def logData(self):
+    def __gromacs(self):
+        os.system('echo arg1 arg2 | gmx trjconv -f {traj_comp.part000*} -o {combine_traj.xtc}'.format(other, arguments))
+
+    def __awk(self):
+        with open("distance.xvg","r+") as f:
+            data=f.read().splitlines(True)
+        with open("dist_trial.log","w") as fout:
+            fout.writelines(data[17:])
+        with open("dist_trial.log","r+") as g:
+            data=g.read()
+            print(data)
+            data=data.replace('\n',';')
+            data=data.replace(' ',',')
+            data=data.strip(',;')
+        distance_values = np.matrix(data)
+        self.distance_values=distance_values
+    
+
+    def __logData(self):
         #use this when in training directory
         save_data=np.zeros((3,3))
         count=0
@@ -59,42 +66,8 @@ class Analysis:
                 save_data[count,1]=data[0,2] #R
                 count=count+1
         self.data=save_data
-        
-
-    def binSet(self):
-        #to be ran after all of the mem_directories have gone through 
-        data=self.data
-        W_matrix=self.W_matrix
-        name_dict=str(data[0,1])+"_"+str(data[1,1])+"_"+str(data[2,1])+".dat"
-        if name_dict in self.analysis_dir:
-            with open(name_dict,"a+") as g:                
-                g.write('%f' % W_matrix)
-            g.close()
-        else: 
-            with open(name_dict,"w+") as g:
-                g.write("Work for each DEER distribution in kJ\n")
-                g.write('%f' % W_matrix)
-            g.close()
-
-    def memSearch(self):
-        path="../mem_{}.".format(self.n)     
-        os.chdir(path)
-        
-    def awk(self):
-        with open("distance.xvg","r+") as f:
-            data=f.read().splitlines(True)
-        with open("dist_trial.log","w") as fout:
-            fout.writelines(data[17:])
-        with open("dist_trial.log","r+") as g:
-            data=g.read()
-            print(data)
-            data=data.replace('\n',';')
-            data=data.replace(' ',',')
-            data=data.strip(',;')
-        distance_values = np.matrix(data)
-        self.distance_values=distance_values
-
-    def workCalc(self):
+    
+    def __workCalc(self):
         distance_values=self.distance_values
         n=distance_values.shape[0]
         data=self.data
@@ -117,3 +90,39 @@ class Analysis:
             W_matrix=[W_DEER1,W_DEER2,W_DEER3]
             W_matrix=np.matrix(W_matrix)
             self.W_matrix=W_matrix
+    
+
+    def __binSet(self):
+        #to be ran after all of the mem_directories have gone through 
+        data=self.data
+        W_matrix=self.W_matrix
+        name_dict=str(data[0,1])+"_"+str(data[1,1])+"_"+str(data[2,1])+".dat"
+        if name_dict in self.analysis_dir:
+            with open(name_dict,"a+") as g:                
+                g.write('%f' % W_matrix)
+            g.close()
+        else: 
+            with open(name_dict,"w+") as g:
+                g.write("Work for each DEER distribution in kJ\n")
+                g.write('%f' % W_matrix)
+            g.close()
+
+        
+    def run(self):
+        n=self.n
+        m=self.m
+        for i in range(n):
+            for j in range(m):
+                self.n=i
+                self.m=j
+                path="../mem_{}/{}".format(self.n,self.m)
+                if os.path.exists(path):
+                    os.chdir(path)
+                    self.__gromacs
+                    self.__awk
+                    self.__logData
+                    self.__workCalc
+                    self.__binSet
+
+                else:
+                    pass #do nothing because mem directory or iteration does not exist
