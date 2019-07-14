2c6748006eec6498e225c87fe3a62c8e139e82b6
Kasey Freshwater <kaf0005@mix.wvu.edu>
2019-07-14 13:14:34 -0400

    working on directory

--- run_analysis.py
+++ run_analysis.py
@@ -8,7 +8,6 @@ import sys
 
 
 init = {
-<<<<<<< Updated upstream
     'tpr': '/home/kaf0005/UVA_reu/analysis/syx.tpr',
     'index_file': '/home/kaf0005/UVA_reu/analysis/beta_carbon_index.ndx',
     'select' : [19, 20, 21],
@@ -17,13 +16,6 @@ init = {
     'analysis_dir': '/home/kaf0005/UVA_reu/analysis',
     'n':[1,2,3],
     'm':[0,1]
-=======
-    'ensemble_dir': '/home/kaf0005/KF2019REU/jmh_dc3_runs',
-    'pairs_json': '/home/kaf0005/KF2019REU/jmh_dc3_runs/analysis/pair_data.json',
-    'analysis_dir': '/home/kaf0005/KF2019REU/jmh_dc3_runs/analysis',
-    'n':[1],
-    'm':[0]
->>>>>>> Stashed changes
 }
 
 
