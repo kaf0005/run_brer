589b8c2cf8dbbb7e08639a933e3a5514eb2fdb0f
Kasey Freshwater <kaf0005@mix.wvu.edu>
2019-07-19 11:01:20 -0400

    debug files

---
+++ run_debug.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+Example run script for BRER simulations
+"""
+
+import run_brer.debug as rc
+import sys
+
+
+init = {
+    'tpr': '/home/kaf0005/KF2019REU/syntaxin_files/syx.tpr',
+    'ensemble_dir': '/home/kaf0005/KF2019REU/jmh_dc3_runs',
+    'ensemble_num': 8,
+    'pairs_json': '/home/kaf0005/KF2019REU/syntaxin_files/pair_data.json',
+    'dict_json': 0
+}
+
+config = rc.Run(**init)
+config.datDict()
+
