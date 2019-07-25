6b34c6da63e71d60507c444c64f49add35a8b3ad
Kasey Freshwater <kaf0005@pmks09.med.virginia.edu>
2019-07-25 16:29:02 -0400

    included boltzmann constant and gromacs command line tools

--- run_analysis.py
+++ run_analysis.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 """
-Example run script for BRER simulations
+Example analysis script for Correlation Structure simulations
+
+To run this script, you must use the gmxapi software that includes gromacs command line operations
 """
 
 import run_brer.mem_search as ms
@@ -10,7 +12,7 @@ import sys
 init = {
     'tpr': '/home/kaf0005/UVA_reu/analysis/syx.tpr',
     'index_file': '/home/kaf0005/UVA_reu/analysis/beta_carbon_index.ndx',
-    'select' : [19, 20, 21],
+    'select' : '25;26;27',
     'ensemble_dir': '/home/kaf0005/UVA_reu/test-brer',
     'pairs_json': '/home/kaf0005/UVA_reu/analysis/pair_data.json',
     'analysis_dir': '/home/kaf0005/UVA_reu/analysis',
--- run_brer/mem_search.py
+++ run_brer/mem_search.py
@@ -7,14 +7,18 @@ import json
 import numpy as np 
 import sys
 import datetime
-import codecs
+import gmxapi as gmx 
+import glob
 
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
 
 class Analysis: 
 
-    def __init__(self,
-                tpr,
-                index_file,
+    def __init__(self,tpr,index_file,
                 select,
                 ensemble_dir,
                 analysis_dir,
@@ -22,7 +26,7 @@ class Analysis:
                 m,
                 pairs_json='pair_data.json',
                 data=[],
-                W_matrix=[],
+                boltzman=[],
                 distance_values=[],
                 end_signal=[]
                 ):
@@ -33,40 +37,64 @@ class Analysis:
         self.analysis_dir=analysis_dir
         self.n=n
         self.m=m
-        self.__names=[]
+
         self.pairs = MultiPair()
         self.pairs.read_from_json(pairs_json)
         self.__names = self.pairs.names
-        self.state_json = '{}/mem_{}/state.json'.format(self.ensemble_dir,self.n)
+
+        
         self.data=[]
-        self.W_matrix=[]
         self.distance_values=[]
         self.end_signal=0
 
-    def __gromacs(self):
-        import gromacs
-        path="{}/mem_{}/{}/convergence".format(self.ensemble_dir, self.n,self.m) 
+        self.run_data = RunData()
+
+    def __gromacs(self,i,j):
+        path="{}/mem_{}/{}/convergence".format(self.ensemble_dir, i,j) 
         os.chdir(path) #convergence directory 
         list=os.listdir()
-        traj=[]
         for names in list:
             if names.endswith(".xtc"):
-                traj.append(names)
+                traj=glob.glob("{}/*.xtc".format(path))
         if not traj:
             self.end_signal=1
         else:
-            combined_traj="{}/combined_traj.xtc".format(self.analysis_dir)
-            gromacs.trjcat(f=traj, o = combined_traj)
-            path="{}".format(self.analysis_dir)
+            trjcat=gmx.commandline_operation(
+                'gmx',
+                'trjcat',
+                input_files={'-f': traj},
+                output_files={'-o': '{}/trjcat.xtc'.format(self.analysis_dir)}
+                )
+            trjcat.run()
+        
+            path='{}'.format(self.analysis_dir)
             os.chdir(path) #analysis directory
             path=self.index_file
             if os.path.exists(path):
-                gromacs.distance(f="combined_traj.xtc", s=self.tpr, n=self.index_file, oall="distance.xvg", select=self.select)
-            else: 
-                gromacs.distance(f="combined_traj.xtc", s=self.tpr, oall="distance.xvg")
+                distance=gmx.commandline_operation(
+                    'gmx',
+                    'distance',
+                    input_files={
+                            '-f': 'trjcat.xtc',
+                            '-s': '{}'.format(self.tpr), 
+                            '-n': '{}'.format(self.index_file),
+                            '-select': '{}'.format(self.select)},
+                    output_files={'-oall':'distance.xvg'} 
+                    )
+                distance.run()                
+            else:
+                distance=gmx.commandline_operation(
+                    'gmx',
+                    'distance',
+                    input_files={
+                            '-f': 'combined_traj.xtc',
+                            '-s': '{}'.format(self.tpr)},
+                    output_files={'-oall':'distance.xvg'} 
+                    )   
+                distance.run()
                 
-
     def __awk(self):
+        # this removes the unnecessary gromacs header in the .xvg file and writes just the data portion to dist.log
         path="{}".format(self.analysis_dir)
         os.chdir(path)
         with open("distance.xvg","r+") as f:
@@ -81,78 +109,95 @@ class Analysis:
         distance_values = np.matrix(data)
         self.distance_values=distance_values
         os.remove("distance.xvg")
-        os.remove("combined_traj.xtc")
+        os.remove("trjcat.xtc")
     
 
-    def __logData(self):
+    def __logData(self,i,j):
+        #logData saves the target, R and alpha from the log files in training directories
         save_data=[]
-        count=0
-        path="{}/mem_{}/{}/training".format(self.ensemble_dir, self.n, self.m) #training directory
+        path="{}/mem_{}/{}/training".format(self.ensemble_dir, i, j) #training directory
         os.chdir(path)
         for name in self.__names:
             with open("{}.log".format(name),"r+") as f:
                 data=f.readlines()[-1]
                 data=data.replace(' ',',')
                 data=np.matrix(data)
-                save_data.append(data[0,2]) 
-                save_data.append(data[0,1]) 
-                save_data.append(data[0,4]) 
-                count=count+1
+                save_data.append(data[0,3]) #target (nm)
+                save_data.append(data[0,1]) #R (nm)
+                save_data.append(data[0,5]) #alpha (kJ/mol)
         save_data=np.matrix(save_data)    
         self.data=save_data
            
     
     def __workCalc(self):
+        #This calculates the work in kJ/mol
+        work={}
+        RT=2.479 #kJ/mol;
         distance_values=self.distance_values
-        distance_values=np.matrix(distance_values)
+        #distance_values=np.matrix(distance_values)
         n=distance_values.shape[0]
-        data=self.data
         n=n-1
-        W_DEER1=0
-        W_DEER2=0
-        W_DEER3=0
-        for i in range (0,n):
-            Sum1=data[0,2]*(distance_values[i+1,1]-distance_values[i,1])
-            W_DEER1 =W_DEER1+Sum1
-            Sum2=data[0,5]*(distance_values[i+1,2]-distance_values[i,2])
-            W_DEER2=W_DEER2+Sum2
-            Sum3=data[0,8]*(distance_values[i+1,3]-distance_values[i,3])
-            W_DEER3=W_DEER3+Sum3
+        data=self.data
+        m=0
+        i=2
+        j=0
+        for name in self.__names:
+            m=m+1
+            alpha=data[0,i]
+            target=data[0,j]
+            force_constant=alpha/target #(kJ/mol)/nm
+            i=i+3
+            j=j+3
+            W_DEER=0
+            for k in range (0,n):
+                delta_R=np.sum(np.abs(distance_values[k+1,m]-distance_values[k,m]))
+                W_DEER =W_DEER+Sum
+            W_DEER=force_constant*W_DEER
+            work[name] =W_DEER
 
-            W_DEER1=(W_DEER1)*1e-12
-            W_DEER2=(W_DEER2)*1e-12
-            W_DEER3=(W_DEER3)*1e-12
-            
-            W_matrix=[W_DEER1,W_DEER2,W_DEER3]
-            W_matrix=np.matrix(W_matrix)
-            self.W_matrix=W_matrix
+        #Calculating boltzmann
+        z=np.sum(list(work.values()))
+        self.boltzman=np.exp((-z)/RT)
+
+        #Printing the work values on the command line
+        data=self.data
+        targetSet='{:2f}_{:2f}_{:2f}'.format(data[0,0],data[0,3],data[0,6])
+        print('Work in kJ/mol for targetset: {}'.format(targetSet))
+        print(z)
 
     def __datDict(self):         
         # Reads in dictionary, if there are no values it sets up the nested dictionary
         data=self.data
         targetSet='{:2f}_{:2f}_{:2f}'.format(data[0,0],data[0,3],data[0,6])
-        workCalc=np.sum(self.W_matrix)
         path = '{}/targetSet.json'.format(self.analysis_dir)
         if os.path.exists(path):
             if os.path.getsize(path)>0:
                 with open('{}/targetSet.json'.format(self.analysis_dir), "r+") as f:
                     dict=json.load(f)
-                if '{}'.format(targetSet) in dict:
-                    values=dict.get('{}'.format(targetSet))
-                    values=np.array(values)
-                    values=np.append(values,workCalc)
-                    dict['{}'.format(targetSet)]=values
-                else:
-                    dict['{}'.format(targetSet)]=workCalc
+                    if targetSet in dict.keys():
+                        values=dict.get('{}'.format(targetSet))
+                        values=np.array(values)
+                        values=np.append(values,self.boltzman)
+                        dict['{}'.format(targetSet)]=values
+                        j=json.dumps(dict,cls=NumpyEncoder)
+                    else:
+                        dict['{}'.format(targetSet)]=self.boltzman
+                        j=json.dumps(dict)
             else:
                 dict={}
-                dict['{}'.format(targetSet)]=workCalc
-        with open('{}/targetSet.json'.format(self.analysis_dir), "w+") as f:
+                dict['{}'.format(targetSet)]=self.boltzman
+                j=json.dumps(dict)
+        else:
+            dict={}
+            dict['{}'.format(targetSet)]=self.boltzman
             j=json.dumps(dict)
-            f.write(j)      
+                
+        with open('{}/targetSet.json'.format(self.analysis_dir), "w+") as f:
+            f.write(j)
+                
 
-    def __analysisLog(self):
-        path=('{}/mem_{}/{}'.format(self.ensemble_dir,self.n,self.m))
+    def __analysisLog(self,i,j):
+        path=('{}/mem_{}/{}'.format(self.ensemble_dir,i,j))
         os.chdir(path)
         now=datetime.datetime.now()
         with open("analysis.log","w+") as f:
@@ -162,24 +207,21 @@ class Analysis:
             f.write(now.strftime("%H:%M"))
 
     def run(self):
-        n=self.n
-        m=self.m
-        for i in n:
-            for j in m:
-                self.n=i
-                self.m=j
-                path="{}/mem_{}/{}/convergence/md.part0001.log".format(self.ensemble_dir,self.n,self.m)
+        for i in self.n:
+            for j in self.m:
+                self.state_json = '{}/mem_{}/state.json'.format(self.ensemble_dir,i)
+                path="{}/mem_{}/{}/convergence/md.part0001.log".format(self.ensemble_dir,i,j)
                 if os.path.exists(path):
-                    exists=os.path.isfile('{}/mem_{}/{}/analysis.log'.format(self.ensemble_dir,self.n,self.m))
+                    exists=os.path.isfile('{}/mem_{}/{}/analysis.log'.format(self.ensemble_dir,i,j))
                     if exists:
                         print("You have already done analysis on this iteration:")
-                        print('{}/mem_{}/{}'.format(self.ensemble_dir,self.n,self.m))
+                        print('{}/mem_{}/{}'.format(self.ensemble_dir,i,j))
                         print("If you wish to do another analysis run on this mem_directory and iteration,")
                         print("please delete the analysis.log file within the iteration directory.")
                         print("\n")
                         break 
                     else:   
-                        self.__gromacs()
+                        self.__gromacs(i,j)
                         if self.end_signal==1:
                             print("There were no xtc files found in the following mem_directory:")
                             cwd=os.getcwd()
@@ -189,13 +231,12 @@ class Analysis:
                             break
 
                         self.__awk()
-                        self.__logData()
+                        self.__logData(i,j)
                         self.__workCalc()
                         self.__datDict()
-                        self.__analysisLog()
+                        self.__analysisLog(i,j)
                 else:
                     print("There are no md log files in the convergence folder for the current iteration:")
                     print(path)
                     print("You may need to do another ./run.py to continue that run.")
                     print("\n")
-                    pass #do nothing because mem directory or iteration does not exist
